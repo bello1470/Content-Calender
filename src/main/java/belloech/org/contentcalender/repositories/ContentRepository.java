9e6dfc2d0f2722cabead8e185fac098947668e6d
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-02-18 13:16:41 +0000

    ContentRepository commits

--- src/main/java/belloech/org/contentcalender/repositories/ContentRepository.java
+++ src/main/java/belloech/org/contentcalender/repositories/ContentRepository.java
@@ -1,64 +1,12 @@
 package belloech.org.contentcalender.repositories;
 
 import belloech.org.contentcalender.models.Content;
-import belloech.org.contentcalender.models.Status;
-import belloech.org.contentcalender.models.Type;
-import jakarta.annotation.PostConstruct;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.IntStream;
-
-@Repository
-public class ContentRepository {
-
-    public List<Content> contents = new ArrayList<>();
-
-    public ContentRepository(){
-
-    }
-
-    public List<Content> findAll(){
-
-        return contents;
-    }
-    public Optional<Content> findById (Integer id){
-        return contents.stream().filter(c -> c.id().equals(id)).findFirst();
-    }
-
-    public void save(Content content){
-        contents.add(content);
 
 
-    }
-
-    @PostConstruct
-    private void init(){
-        Content c = new Content(
-                1,"My first post",
-                "my first blog post",
-                Status.IDEA,
-                Type.ARTICLE,
-                LocalDateTime.now(),
-                null,
-                "");
+public interface ContentRepository extends ListCrudRepository<Content, Integer> {
 
 
-contents.add(c);
-
-    }
-
-    public boolean existById(Integer id) {
-        return contents.stream().filter(c->c.id().equals(id)).count()==1;
-      
-    }
-
-    public void deleteById (Integer id){
-
-        contents.removeIf(c->c.id().equals(c.id()));
-
-    }
 }
