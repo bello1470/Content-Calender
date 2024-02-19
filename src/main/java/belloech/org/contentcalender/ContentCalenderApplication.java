69cba1bf58f00f6fc1d881cfe58e835354b9f7f9
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-02-19 18:45:04 +0000

    profile setup commits

--- src/main/java/belloech/org/contentcalender/ContentCalenderApplication.java
+++ src/main/java/belloech/org/contentcalender/ContentCalenderApplication.java
@@ -1,11 +1,14 @@
 package belloech.org.contentcalender;
 
+import belloech.org.contentcalender.config.ContentCalenderProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ContentCalenderProperties.class)
 public class ContentCalenderApplication {
 
 	public static void main(String[] args) {
