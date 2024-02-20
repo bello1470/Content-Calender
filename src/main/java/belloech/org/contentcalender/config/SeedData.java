a49f49b448839b3283178290ee6c80d84a713923
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-02-20 18:29:39 +0000

    Seed Data commits

---
+++ src/main/java/belloech/org/contentcalender/config/SeedData.java
@@ -0,0 +1,35 @@
+package belloech.org.contentcalender.config;
+
+import belloech.org.contentcalender.models.Content;
+import belloech.org.contentcalender.repositories.ContentCollectionRepository;
+import belloech.org.contentcalender.repositories.ContentRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeOverride;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.List;
+
+
+@Component
+public class SeedData implements CommandLineRunner {
+
+    @Autowired
+    private ContentRepository contentRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Override
+    public void run(String... args) throws Exception {
+
+        try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")){
+
+            contentRepository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>(){}));
+        }
+    }
+}
+
