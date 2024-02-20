ee2f5d841a0f082886a027454c48fec723920ed2
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-02-20 18:25:08 +0000

    Home controller commits

--- src/main/java/belloech/org/contentcalender/controller/ContentController.java
+++ src/main/java/belloech/org/contentcalender/controller/ContentController.java
@@ -2,8 +2,8 @@ package belloech.org.contentcalender.controller;
 
 
 import belloech.org.contentcalender.models.Content;
+import belloech.org.contentcalender.repositories.ContentCollectionRepository;
 import belloech.org.contentcalender.repositories.ContentRepository;
-import belloech.org.contentcalender.repositories.ContentTemplateRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,47 +18,46 @@ import java.util.Optional;
 @CrossOrigin
 public class ContentController {
 
-    private final ContentRepository contentRepository;
-
-    public ContentController(ContentRepository contentRepository){
-        this.contentRepository = contentRepository;
-    }
 
     @Autowired
-    private ContentTemplateRepository contentTemplateRepository;
+    private ContentCollectionRepository contentCollectionRepository;
+
+
+
+
 
     //make a request and find all pieces of content in the system
 
     @GetMapping("")
     public List<Content> findAllContent(){
 
-        return contentTemplateRepository.getAllContent();
+        return contentCollectionRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Content findById(@PathVariable Integer id){
 
-        return contentTemplateRepository.getContent(id);
+        return contentCollectionRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found"));
     }
 
     @PostMapping("")
     public void createContent(@Valid @RequestBody Content content){
 
-        contentRepository.save(content);
+        contentCollectionRepository.save(content);
     }
 
     @PutMapping("/{id}")
     public void updateContentById(@RequestBody Content content, @PathVariable Integer id){
-        if (!contentRepository.existById(id)){
+        if (!contentCollectionRepository.existById(id)){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found");
         }
-        contentRepository.save(content);
+        contentCollectionRepository.save(content);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
         public void deleteContentById(@PathVariable Integer id){
-        contentRepository.deleteById(id);
+        contentCollectionRepository.deleteById(id);
     }
 
 
